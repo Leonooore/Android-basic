c3fb14c0fd37db5e1c9ef38c888bf819a518981a
Leonooore <eelyannora13@gmail.com>
2020-07-18 15:27:39 +0300

    mainview initial commit

---
+++ MainView/app/src/main/java/com/gmail/elnora/fet/mainview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.elnora.fet.mainview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
